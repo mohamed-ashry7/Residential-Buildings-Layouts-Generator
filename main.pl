dd2c48a7137714dac895283916c31bdada9b6921
mohamed-ashry7 <mohamedelashry7777@gmail.com>
2021-01-11 14:10:47 +0200

    Adding new files

--- main.pl
+++ main.pl
@@ -1,58 +1,222 @@
 :-use_module(library(clpfd)).
 
 
+floor_width(100).
+floor_height(200).
 
-room_type()
+open_sides(1,1,0,1).
+%          N,E,S,W
+
+
+
+
+
+get_room_type(bedroom,X,W,Y,H,Type):-
+    Type = bedroom(X,W,Y,H).
+
+get_room_type(kitchen,X,W,Y,H,Type):-
+    Type = kitchen(X,W,Y,H).
+
+get_room_type(dining_room,X,W,Y,H,Type):-
+    Type = dining_room(X,W,Y,H).
+
+get_room_type(master_bathroom,X,W,Y,H,Type):-
+    Type = master_bathroom(X,W,Y,H).
+
+get_room_type(minor_bathroom,X,W,Y,H,Type):-
+    Type = minor_bathroom(X,W,Y,H).
+
+get_room_type(living_room,X,W,Y,H,Type):-
+    Type = living_room(X,W,Y,H).
+
+get_room_type(hallway,X,W,Y,H,Type):-
+    Type = hallway(X,W,Y,H).
+
+get_room_type(dressing_room,X,W,Y,H,Type):-
+    Type = dressing_room(X,W,Y,H).
+
+get_room_type(sun_room,X,W,Y,H,Type):-
+    Type = sun_room(X,W,Y,H).
+
+
 convert_types_into_rooms([],[],[]).
-convert_types_into_rooms([Room|RS],[R|T],[X,Width,Y,Height|Vs]):-
-    ((length(Room,2),Room =[Type,Area]);
-    (length(Room,3),Room = [Type,Area,Width]);
-    (length(Room,4),Room = [Type, Area,Width,Height])),
-    [X,Width,Y,Height] ins 1..1000, % TODO 
-    Width*Height #>=Area,
-    R = room(X,Width,Y,Height),
-    convert_types_into_rooms(RS,T,Vs).
+convert_types_into_rooms([Room|RS],W,H,[R|T],[X,Width,Y,Height|Vs]):-
+    ((length(Room,2),Room =[Type,MinArea]);
+    (length(Room,3),Room = [Type,MinArea,Width]);
+    (length(Room,4),Room = [Type, MinArea,Width,Height])),
+    X in 0..W,
+    Y in 0..H,
+    Width in 1..W,
+    Height in 1..H,
+    X+Width #=< W,
+    Y+Height #=<H,
+    RealArea #= Width*Height,
+    RealArea #>=MinArea,
+    get_room_type(Type,X,W,Y,H,R),
+    convert_types_into_rooms(RS,W,H,T,Vs).
 
 
 
-find(L,F):-
-    convert_types_into_rooms(L,F,Vs),
+
+
+
+% ------------------------------------------Adjacency predicate----------------------------------------
+make_adjacent(U,V):-
+    get_room_type(_,X1,W1,Y1,H1,U),
+    get_room_type(_,X2,W2,Y2,H2,V),
+    (
+    X2 #= X1+W1;
+    X1 #= X2+W2;
+    Y2 #= H1+Y1;
+    Y1 #= H2+Y2
+    ).
+make_adjacent_lists(_,[]).
+make_adjacent_lists(U,[V|Vs]):-
+    make_adjacent(U,V),
+    make_adjacent_lists(U,Vs).
+
+
+% -----------------------------------------The kitchens and Bathrooms-----------------------------------------------------------
+get_the_kitchen([H|T],K):-
+    H \= kitchen(_,_,_,_),
+    get_the_kitchen(T,K).
+
+get_the_kitchen([H|_],H):-
+    H = kitchen(_,_,_,_).
+
+get_the_bathrooms([],[]).
+get_the_bathrooms([H|T],T1):-
+    H \= master_bathroom(_,_,_,_),
+    H \= minor_bathroom(_,_,_,_),
+    get_the_bathrooms(T,T1).
+
+get_the_bathrooms([H|T],[H|T1]):-
+    (H = master_bathroom(_,_,_,_);
+    H = minor_bathroom(_,_,_,_)),
+    get_the_bathrooms(T,T1).
+
+
+make_the_kitchen_bathrooms_adjacent(L):-
+    get_the_kitchen(L,K),
+    get_the_bathrooms(L,Bs),
+    make_adjacent_lists(K,Bs).
+
+
+%-------------------------------------------------------Sun room --------------------------------------------------------------------------
+
+get_the_sun_room([],none). % In case there is not a dining room. 
+
+get_the_sun_room([H|T],K):-
+    H \= sun_room(_,_,_,_),
+    get_the_sun_room(T,K).
+
+get_the_sun_room([H|_],H):-
+    H = sun_room(_,_,_,_).
+
+
+make_the_sun_room_light(L,O):-
+    get_the_sun_room(L,S),
+    S = sun_room(X,_,Y,_),
+    open_sides(N,E,S,W),
+    (   S=none;
+        (N=1,O=min(Y));
+        (S=1,O=max(Y));
+        (E=1,O=max(X));
+        (W=1,O=min(X))
+    ).
+
+
+% ------------------------------------------------------Dressing Rooms and Bedrooms----------------------------------------------------------
+get_the_dressing_rooms_bedrooms([],[]).
+
+get_the_dressing_rooms_bedrooms([H|T],T1):-
+    H\=dressing_room(_,_,_,_),
+    get_the_dressing_rooms_bedrooms(T,T1). 
+
+get_the_dressing_rooms_bedrooms([D,B|T],[D,B|T1]):-
+    D = dressing_room(_,_,_,_), 
+    B = bedroom(_,_,_,_),
+    get_the_dressing_rooms_bedrooms(T,T1).
+
+
+make_the_dressing_rooms_bedrooms_adjacent_helper([]).
+make_the_dressing_rooms_bedrooms_adjacent_helper([D,B|T]):-
+    make_adjacent(D,B),
+    make_the_dressing_rooms_bedrooms_adjacent_helper(T).
+
+make_the_dressing_rooms_bedrooms_adjacent(L):-
+    get_the_dressing_rooms_bedrooms(L,T),
+    make_the_dressing_rooms_bedrooms_adjacent_helper(T).
+
+
+
+
+
+%------------------------------------------------Minor Bathrooms and Bedrooms-------------------------------------
+
+get_the_bedrooms_minor_bathrooms([],[]).
+
+get_the_bedrooms_minor_bathrooms([H|T],T1):-
+    H\=bedroom(_,_,_,_),
+    get_the_bedrooms_minor_bathrooms(T,T1). 
+
+get_the_bedrooms_minor_bathrooms([B,M|T],[B,M|T1]):-
+B = bedroom(_,_,_,_), 
+M = minor_bathroom(_,_,_,_),
+get_the_bedrooms_minor_bathrooms(T,T1).
+
+
+make_the_bedrooms_minor_bathrooms_adjacent_helper([]).
+make_the_bedrooms_minor_bathrooms_adjacent_helper([B,M|T]):-
+    make_adjacent(B,M),
+    make_the_bedrooms_minor_bathrooms_adjacent_helper(T).
+
+make_the_bedrooms_minor_bathrooms_adjacent(L):-
+    get_the_dressing_rooms_bedrooms(L,T),
+    make_the_bedrooms_minor_bathrooms_adjacent_helper(T).
+%-------------------------------------------------Dining Room and Kitchen------------------------------------------
+
+
+
+get_the_dining_room([],none). % In case there is not a dining room. 
+
+get_the_dining_room([H|T],K):-
+    H \= dining_room(_,_,_,_),
+    get_the_dining_room(T,K).
+
+get_the_dining_room([H|_],H):-
+    H = dining_room(_,_,_,_).    
+
+
+make_the_kitchen_dining_room_adjacent(L):-
+    get_the_kitchen(L,K),
+    get_the_dining_room(L,D),
+    (
+    (D \=none , make_adjacent(K,D));
+        D = none
+    ).
+
+
+
+
+%--------------------------------------------------Design Apartment----------------------------------------------
+
+
+design_apart(L,W,H,F):-
+    convert_types_into_rooms(L,W,H,F,Vs),
+    make_the_kitchen_bathrooms_adjacent(L),
+    make_the_sun_room_light(L,O),
+    make_the_dressing_rooms_bedrooms_adjacent(L),
+    make_the_bedrooms_minor_bathrooms_adjacent_helper(L),
+    make_the_kitchen_dining_room_adjacent(L),
     disjoint2(F),
+    labeling([O],Vs).
     
-    labeling([],Vs).
-    
-    
-% solve(Area,OpenSides,Apartments,ApartmentsNumber):-
-
-%     Area = [Width,Height],
 
 
-% solve(Area,SidesN,Sides,TypesN,Types,RperT,AperT):-  
-%     % Area [width,height],
-%     % SidesN=>    number of open sides
-%     % Sides=> The sides that are open 
-%     % typesN=>    Number of needed types
-%     % Types=> list of lists of rooms specifying the different rooms per type
-%     % RperT=> Number of rooms per type
-%     % AperT=> number of appartments per type.
-%     Area=[W,H],
-%     %Area ins 1..sub,
-%     length(Sides,SidesN),
-%     Sides ins 1..4,% 1=>North,2=>south,3=>east,4=>west.
-%     length(Types,TypesN),
-%     %Type of rooms: (Bedroom, Kitchen, Dining Room, Master bathroom, Minor bathroom, Living Room, Hallway, Dressing room and Sun room);
-%     %room given a value  1       2       3               4       5       6                  7        8           9               10
-%     length(RperT,TypesN),
-%     length(AperT,TypesN),
-%     rooms_per_type(Types,RperT).  % check that every type has its corresponding number of rooms. rooms_per_type([[A],[B,C],[D,E,F]],[1,2,3]) evaluates to true.
-
-%     %RperT ins 1..sub,
-%     %AperT ins 1..sub.
-
-    
-% rooms_per_type([],[]).
-% rooms_per_type([H|T],[H1|T1]):-
-%     length(H,H1),
-%     H ins 1..10,                        %the different types of rooms 
-%     rooms_per_type(T,T1).
-
+test(F):-
+    Rs = [R1,R2,R3],
+    R1=[bedroom,50],
+    R2=[minor_bathroom,20] ,
+    R3=[kitchen,10],
+    design_apart(Rs,100,200,F).
