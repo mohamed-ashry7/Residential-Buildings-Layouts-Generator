57054c46c5b520d5d5b097bd6b3d2dc360911fe5
mohamed-ashry7 <mohamedelashry7777@gmail.com>
2021-01-14 18:20:52 +0200

    Finishing hard and global optional constraints

--- main.pl
+++ main.pl
@@ -1,11 +1,7 @@
 :-use_module(library(clpfd)).
 
 
-floor_width(100).
-floor_height(200).
 
-open_sides(1,1,0,1).
-%          N,E,S,W
 
 
 
@@ -48,8 +44,9 @@ get_room_type(sun_room,X,W,Y,H,Type):-
     Type = sun_room(X,W,Y,H).
 
 
-convert_types_into_rooms([],_,_,[],[]).
-convert_types_into_rooms([Room|RS],W,H,[R|T],[X,Width,Y,Height|Vs]):-
+convert_types_into_rooms([],[],[]).
+convert_types_into_rooms([Room|RS],[R|T],[X,Width,Y,Height|Vs]):-
+    floor_dimensions(W,H),
     ((length(Room,2),Room =[Type,MinArea]);
     (length(Room,3),Room = [Type,MinArea,Width]);
     (length(Room,4),Room = [Type, MinArea,Width,Height])),
@@ -60,7 +57,7 @@ convert_types_into_rooms([Room|RS],W,H,[R|T],[X,Width,Y,Height|Vs]):-
     RealArea #= Width*Height,
     RealArea #>=MinArea,
     get_room_type(Type,X,Width,Y,Height,R),
-    convert_types_into_rooms(RS,W,H,T,Vs).
+    convert_types_into_rooms(RS,T,Vs).
 
 
 
@@ -123,23 +120,19 @@ get_the_sun_room([H|_],H):-
     H = sun_room(_,_,_,_).
 
 
-make_the_sun_room_light(L,O):-
-    get_the_sun_room(L,S),
-    (
-    (S=none, O=none);
-
-    (S = sun_room(X,_,Y,_),
+make_a_room_on_landscape(R,O):-
+    get_room_type(_,X,Wi,Y,He,R),
     open_sides(N,E,S,W),
     (
         (N=1,O=min(Y));
-        (S=1,O=max(Y));
-        (E=1,O=max(X));
-        (W=1,O=min(X));
-        O = none
-    )
-    )
-    ).
-
+        (S=1,O=max(Y+He));
+        (E=1,O=max(X+Wi));
+        (W=1,O=min(X))
+    ). 
+make_the_sun_room_light(L,O):-
+    get_the_sun_room(L,S),
+    ((S=none, O=none);
+    make_a_room_on_landscape(S,O)).
 
 % ------------------------------------------------------Dressing Rooms and Bedrooms----------------------------------------------------------
 get_the_dressing_rooms_bedrooms([],[]).
@@ -265,9 +258,9 @@ get_one_hallway([H|T],G):-
 %-------------------------------------------------Design Apartment----------------------------------------------
 
 
-design_apart(L,W,H,F,Vs,Options,Hallway):-
+design_apart(L,F,Vs,Options,Hallway):-
     handle_hallway(L,NL),
-    convert_types_into_rooms(NL,W,H,F,Vs),
+    convert_types_into_rooms(NL,F,Vs),
     get_one_hallway(F,Hallway),
     make_the_kitchen_bathrooms_adjacent(F),
     make_the_sun_room_light(F,O),
@@ -278,15 +271,16 @@ design_apart(L,W,H,F,Vs,Options,Hallway):-
     make_all_rooms_adjacent_hallway(F).
     
 
-design_floor_helper([],_,_,[],[],[],[]).
-design_floor_helper([H|T],Width,Height,[F|T1],[Vs|T2],[Options|T3],[Hallway|T4]):-
-    design_apart(H,Width,Height,F,Vs,Options,Hallway),
-    design_floor_helper(T,Width,Height,T1,T2,T3,T4).
+design_floor_helper([],[],[],[],[]).
+design_floor_helper([H|T],[F|T1],[Vs|T2],[Options|T3],[Hallway|T4]):-
+    design_apart(H,F,Vs,Options,Hallway),
+    design_floor_helper(T,T1,T2,T3,T4).
 
 
 
 
-add_elevator_stairs(W,H,ElevatorStairs,Coordinates):-
+add_elevator_stairs(ElevatorStairs,Coordinates):-
+    floor_dimensions(W,H),
     ElevatorStairs =[elevator(XE,1,YE,1),stairs(XS,1,YS,1)],
     Coordinates= [XE,YE,XS,YS],
     [XE,XS] ins 0..W,
@@ -297,7 +291,8 @@ add_elevator_stairs(W,H,ElevatorStairs,Coordinates):-
     YS +1 #=<H.
 
 
-add_main_hallway(Width,Height,MainHallWay,MainHallWayVars):-
+add_main_hallway(MainHallWay,MainHallWayVars):-
+    floor_dimensions(Width,Height),
     MainHallWay = main_hallway(MX,MW,MY,MH),
     MainHallWayVars = [MX,MW,MY,MH],
     [MX,MW] ins 0..Width,
@@ -316,41 +311,160 @@ get_the_whole_area([H|T],A):-
     A #= G+A1,
     get_the_whole_area(T,A1).
 
-option_divine_propotion(_,[]).
-option_divine_propotion(Width,[R|T]):-
+option_divine_propotion([]).
+option_divine_propotion([R|T]):-
+    floor_dimensions(Width,_),
     get_room_type(_,_,W,_,H,R),
     (var(W),
     var(H),
     B in Width,
     H+B#=W,
     W//H #= H//B;true),
-    option_divine_propotion(Width,T). 
+    option_divine_propotion(T). 
 
 
-design_floor(As,Width,Height,OptionalGlobal,F):-
-    [LandscapeLook,DistanceToElevator,Symmetry,Divine] = OptionalGlobal, 
-    add_main_hallway(Width,Height,MainHallWay,MainHallWayVars),
-    add_elevator_stairs(Width,Height,ES,Coor),
-    make_adjacent_lists(MainHallWayVars,ES),
-    design_floor_helper(As,Width,Height,FF,VF,OF,Hallways),
+has_sunroom([R|_]):-
+    R =sun_room(_,_,_,_).
+has_sunroom([R|T]):-
+    R\=sun_room(_,_,_,_),
+    has_sunroom(T).
+
+make_a_apart_on_landscape([],_).
+make_a_apart_on_landscape([R|T],O):-
+    make_a_room_on_landscape(R,O);make_a_apart_on_landscape(T,O).
+
+
+option_landscape([],[]).
+
+option_landscape([A|T],[O|T1]):-
+    (has_sunroom(A);\+has_sunroom(A),make_a_apart_on_landscape(A,O)),
+    option_landscape(T,T1).
+
+
+option_equal_distance_to_elevator_helper([],_,[]).
+
+option_equal_distance_to_elevator_helper([A|T],E,[D|T1]):-
+    get_one_hallway(A,Hall),
+    get_room_type(_,X1,_,Y1,_,E),
+    get_room_type(_,X2,W,Y2,H,Hall),
+    D #= (X1-(X2+W//2))^2 + (Y1-(Y2+H//2))^2,
+    option_equal_distance_to_elevator_helper(T,E,T1).
+    
+option_equal_distance_to_elevator(As,E):-
+    option_equal_distance_to_elevator_helper(As,E,Ds),
+    all_the_same(Ds).
+
+all_the_same([_]).
+all_the_same([A,B|T]):-
+    A #=B,
+    all_the_same([B|T]).  
+
+
+option_symmetry([]).
+
+handle_optional_global_constraints(F,Options):-
+    optional_global(LandscapeLook,DistanceToElevator,Symmetry,Divine), 
+    [[E,_],_|Apartments] = F,
+    flatten(F, G),
+    (LandscapeLook =1, option_landscape(Apartments,Options);LandscapeLook=0,Options=[]),
+    (DistanceToElevator =1, option_equal_distance_to_elevator(Apartments,E);DistanceToElevator=0),
+    (Symmetry =1, option_symmetry([]);Symmetry=0),
+    (Divine =1, option_divine_propotion(G);Divine=0).
+
+
+calc_exposed_fn([],0).
+calc_exposed_fn([R|A],C):-
+    get_room_type(_,X,W,Y,H,R),
+    % open_sides(No,Ea,So,We),
+    floor_dimensions(Width,Height),
+    ((
+    (X<1,We=1;
+    Y<1,No=1;
+    X<Width,X>Width-5,Ea=1;
+    Y<Height,Y>Height-5),
+    C1=0);C1=1),
+    C #= C1+C2,
+    calc_exposed_fn(A,C2). 
+
+get_all_bedrooms([],[]).
+get_all_bedrooms([R|A],[R|T1]):-
+    get_room_type(bedroom,_,_,_,_,R),get_all_bedrooms(A,T1).
+
+get_all_bedrooms([R|A],T1):-
+    get_room_type(Type,_,_,_,_,R),Type\=bedroom,get_all_bedrooms(A,T1). 
+
+
+calc_abs_distance(F,G,C):-
+    get_room_type(_,X,_,Y,_,F),
+    get_room_type(_,X1,_,Y1,_,G),
+    C #= abs(X-X1)+abs(Y-Y1).
+calc_distance_fn([],0).
+
+calc_bedroom_closer([_],0).
+calc_bedroom_closer([F,G|A],C):-
+    calc_abs_distance(F,G,C2),
+    C #= C2+C1,
+    calc_bedroom_closer([G|A],C1).
+
+get_all_bathrooms([],[]).
+get_all_bathrooms([R|A],[R|T1]):-
+    get_room_type(master_bathroom,_,_,_,_,R),get_all_bathrooms(A,T1).
+
+get_all_bedrooms([R|A],T1):-
+    get_room_type(Type,_,_,_,_,R),Type\=master_bathroom,get_all_bathrooms(A,T1). 
+
+calc_bathroom_fn([R|A],[B|Bs],C):-
+    calc_abs_distance(R,B,C1),
+    C #=C1+C2,
+    calc_bathroom_fn(A,Bs,C2).
+
+calc_bathroom_fn(A,C):-
+    get_all_bathrooms(A,Bs),
+    calc_bathroom_fn(A,Bs,C).
+
+
+
+implement_soft_constraints([],[],0).
+
+implement_soft_constraints([A|T1],[S|C],V):-
+    [ExposedDayLight,Distance,BedroomsCloser,MainBathroom] = S, 
+    (ExposedDayLight=1,calc_exposed_fn(A,EV);ExposedDayLight=0,EV=0),
+    (Distance=1,get_all_bedrooms(A,Bs),calc_distance_fn(A,DV);Distance=0,DV=0),
+    (BedroomsCloser=1,get_all_bedrooms(A,Bs),calc_bedroom_closer(A,BV);BedroomsCloser=0,BV=0),
+    (MainBathroom=1,calc_bathroom_fn(A,MBV);MainBathroom=0,MBV=0),
+    V #= EV+DV+BV+MBV+V1,
+    implement_soft_constraints(T1,C,V1).
+
+design_floor(As,SoftConstraints,F):-
+    length(As,NumberApartments),
+    length(SoftConstraints,NumberApartments) , %To make sure every apartment has its soft constraints.
+    implement_soft_constraints(FF,SoftConstraints,CostVars),
+    add_main_hallway(MainHallWay,MainHallWayVars),
+    add_elevator_stairs(ES,Coor),
+    make_adjacent_lists(MainHallWay,ES),
+    design_floor_helper(As,FF,VF,OF,Hallways),
     make_adjacent_lists(MainHallWay,Hallways),
     Vs = [Coor,MainHallWayVars|VF],
     flatten(Vs, Variables),
     F = [ES,MainHallWay|FF],
     flatten(F, Functors),
-    get_the_whole_area(F,Area),
-    OFA = [max(Area)|OF],
+    get_the_whole_area(Functors,Area),
+    handle_optional_global_constraints([ES,MainHallWay|FF],GlobalOptions),
+    OFA = [max(Area),min(CostVars),GlobalOptions|OF],
     flatten(OFA, Options),
-    (Divine =1 -> option_divine_propotion(Width,F)),
     disjoint2(Functors),
     labeling(Options,Variables).
 
 
+floor_dimensions(100,200).
+%            Width,Height
+open_sides(1,1,0,1).
+%          N,E,S,W
+optional_global(1,1,1,1).
+%  [LandscapeLook,DistanceToElevator,Symmetry,Divine]
 
 
 test0(F):-
-    Width = 100,
-    Height = 200,
     As = [A1,A2],
     A1 = [R1,R2,R3],
     R1=[bedroom,50],
@@ -360,8 +474,7 @@ test0(F):-
     R4=[bedroom,10],
     R5=[minor_bathroom,10] ,
     R6=[kitchen,25] ,
-    design_floor(As,Width,Height,[1,1,1,1],F).
-%                               [LandscapeLook,DistanceToElevator,Symmetry,Divine]
+    design_floor(As,Soft,F).
 
 
 test1(Fs):-
@@ -380,17 +493,22 @@ test1(Fs):-
 
 test2(F):-
     Divine = 1 ,
-    W=100,H=200,
     A1 = [R1,R2,R3],
     R2=[bedroom,50],
     R1=[minor_bathroom,20] ,
     R3=[kitchen,10],
-    design_apart(A1,W,H,F,Vs,O,_),
-    (Divine =1,option_divine_propotion(W,F) ;Divine = 0 ),
+    design_apart(A1,F,Vs,O,_),
+    (Divine =1,option_divine_propotion(F) ;Divine = 0 ),
     flatten(O, Options),
     disjoint2(F),
     labeling(Options,Vs).
 
 
+
+test5(F):-
+    F #= (X1-X2)^2,
+    [X1,X2] ins 1..9,
+    all_distinct([X1,X2]).
+    
 % test3(F):-
 
